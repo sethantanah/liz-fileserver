94c34b8f24ca3123928523211ff37f919ac869db
sethantanah <sethsyd32@gmail.com>
2023-04-25 21:27:21 +0000

    Account Registration - Email verification

--- fileserver/accounts/urls.py
+++ fileserver/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth
-from .views import sign_up
+from .views import sign_up, activate, confirm_email
 
 urlpatterns = [
     path('/sign-up', sign_up, name='sign_up'),
+    path('/confirm-email', confirm_email, name='confirm_main'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+         activate, name='activate')
 ]
--- fileserver/accounts/views.py
+++ fileserver/accounts/views.py
@@ -25,7 +25,7 @@ def sign_up(request):
             # to get the domain of the current site
             current_site = get_current_site(request)
             mail_subject = 'Activation link has been sent to your email id'
-            message = render_to_string('acc_active_email.html', {
+            message = render_to_string('verification/acc_active_email.html', {
                 'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
@@ -43,3 +43,24 @@ def sign_up(request):
     else:
         form = RegistrationForm()
     return render(request, 'signup.html', {'form': form})
+
+
+
+def confirm_email(request):
+    return render(request, 'verification/confirm_password.html')
+
+
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return render(request, 'verification/activate_account.html',
+                      {'message': 'Thank you for your email confirmation. Now you can login your account.'})
+    else:
+        return render(request, 'verification/activate_account.html', {'message': 'Activation link is invalid!'})
