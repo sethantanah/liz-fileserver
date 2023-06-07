a57d5ddadd5f10ef67ea7009e62c5acc52458d6a
Seth Antanah <sethsyd32@gmail.com>
2023-06-07 16:36:45 +0000

    Updates to environment settings

--- fileserver/fileserver/settings.py
+++ fileserver/fileserver/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -168,7 +168,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 # Update database configuration from $DATABASE_URL.
-import dj_database_url
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
