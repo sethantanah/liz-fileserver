a5b697124be3ebc34e800bd401375000c62e117d
Seth Antanah <sethsyd32@gmail.com>
2023-06-08 15:10:41 +0000

    Unview production errors

--- fileserver/fileserver/settings.py
+++ fileserver/fileserver/settings.py
@@ -163,11 +163,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
+
 
 # Update database configuration from $DATABASE_URL.
 
