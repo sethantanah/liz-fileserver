7838f15376760956d90c83fd2804eb77ddc11ba3
Seth Antanah <sethsyd32@gmail.com>
2023-06-08 11:57:25 +0000

    updates to firebase storage

--- fileserver/dashboard/firebase_upload.py
+++ fileserver/dashboard/firebase_upload.py
@@ -1,3 +1,28 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+import json
+import os
+
+from firebase_admin import credentials, initialize_app, storage
+from django.core.files.storage import FileSystemStorage
+
+service_account_info = json.loads(os.environ.get('FIREBASE_SERVICE_ACCOUNT_KEY'))
+cred = credentials.Certificate(service_account_info)
+initialize_app(cred, {'storageBucket': 'home-status-c4316.appspot.com'})
+
+
+def uploadfile(file):
+    # Put your local file path
+    fileName = file_path(file)
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+    blob.make_public()
+    print("your file url", blob.public_url)
+
+    return blob.public_url
+
+
+def file_path(file):
+    fs = FileSystemStorage()
+    filename = fs.save(file.name, file)
+    uploaded_file_path = fs.path(filename)
+    return uploaded_file_path
--- fileserver/dashboard/views.py
+++ fileserver/dashboard/views.py
@@ -11,41 +11,7 @@ from library.models import FileTracker
 from library.models import Files
 from accounts.models import User
 
-
-
-
-
-
-config = {
-    'apiKey': "AIzaSyBS2xUVtdq9RvsyhQrzSSuyaQVPbIYA95Y",
-    'authDomain': "home-status-c4316.firebaseapp.com",
-    'projectId': "home-status-c4316",
-    'storageBucket': "home-status-c4316.appspot.com",
-    'messagingSenderId': "1083784871321",
-    'appId': "1:1083784871321:web:f86f3181602153fc562708",
-    'measurementId': "G-MEBSQ6SJGV",
-    "databaseURL": ""
-}
-
-# firebase = pyrebase.initialize_app(config)
-# auth = firebase.auth()
-# storage = firebase.storage()
-
-
-def save_image(file):
-
-
-    return ''
-    # if auth.current_user is not None:
-    #     storage.child("files/" + file.name).put(file)
-    #     return storage.child("files/" + file.name).get_url(auth.current_user['idToken'])
-    # else:
-    #     user = auth.sign_in_with_email_and_password('sethsyd32@gmail.com', os.environ.get('EMAIL_PASSWORD'))
-    #     storage.child("files/" + file.name).put(file)
-    #     return storage.child("files/" + file.name).get_url(user['idToken'])
-
-
-
+from dashboard.firebase_upload import uploadfile
 
 
 @permission_required('user.can_add_files', raise_exception=True)
@@ -90,12 +56,13 @@ def upload_file(request):
         if form.is_valid():
             file = form.save(commit=False)
             file.file = request.FILES['file']
-            file.file_url = save_image(request.FILES['file'])
+            file.file_url = uploadfile(request.FILES['file'])
             file_tracker = FileTracker()
             file_tracker.file = file
             file.save()
             file_tracker.save()
             return redirect(reverse('dashboard'))
+            return render(request, 'add-file.html', {'form': form})
         else:
             return render(request, 'add-file.html', {'form': form})
 
